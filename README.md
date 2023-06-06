84614612adce8dbf5d37d5f8c8cdffb5ad7d41d9
tomhumbert <ht409@outlook.com>
2023-06-06 14:36:34 +0200

    Update after first successful data collection

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 PRAW Reddit scraper with some useful functions for research on reddit.
 (First version by Lauren)
+
+Smart wordlist for stopword removal from:
+Lewis, D. D.; Yang, Y.; Rose, T.; and Li, F. RCV1: A New Benchmark Collection for Text Categorization Research. Journal of Machine Learning Research, 5:361-397, 2004. http://www.jmlr.org/papers/volume5/lewis04a/lewis04a.pdf. 
+
+An terminal based, interactive version is the goal.
+
+For now, tasks have to be defined manually before execution.
--- reddit_scraper.py
+++ reddit_scraper.py
@@ -16,8 +16,8 @@ class RScraper(Reddit):
         """Initialize praw with personal credentials"""
         creds = open(cred_file,'r').read().replace("\n", " ").split()
         super().__init__(client_id= creds[0], client_secret= creds[1], user_agent= creds[2])
-        self.posts = pd.DataFrame(columns=['id', 'title', 'body', 'author', 'is_nsfw', 'n_comments', 'up_ratio', 'date', 'link', 'sub'])
-        self.description = pd.DataFrame(columns=['sub','n_posts','n_comments'])
+        self.posts = pd.DataFrame(columns=['id', 'title', 'body', 'author', 'is_nsfw', 'n_comments', 'up_ratio', 'date', 'link', 'subreddit', 'contained_links'])
+        self.description = pd.DataFrame(columns=['subreddit','n_posts','n_comments','domain_counts'])
 
     def fetch_posts(self, sub, platform, fetchlim):
         """Fetch all posts resulting from a query on given platforms and given keywords.
@@ -28,13 +28,17 @@ class RScraper(Reddit):
         subreddits = False
         query = False
 
+        # param of subreddits is a string
         if type(sub) == str:
+            # The string is a path
             if sub in os.listdir(os.path.curdir):
                 with open(sub, 'r') as s:
                     subreddits = s.readlines()
                     subreddits = [z.replace("\n", "") for z in subreddits]
+            # The string is a subreddit name
             else:
                 subreddits = [sub]
+        # The param is a list of strings
         elif type(sub) == list:
             subreddits = sub
         else:
@@ -50,7 +54,7 @@ class RScraper(Reddit):
             elif platform == "" or platform == " ":
                 query = "r/"
             else:
-                query = '"'+platform+'"'
+                query = platform
         elif type(platform) == list:
             platforms = ['"'+b+'"' for b in platform]
             query = ' OR '.join(platforms)
@@ -62,31 +66,56 @@ class RScraper(Reddit):
             print(f"Now accessing {subreddit}")
             matched_posts = self.subreddit(subreddit).search(query, sort='new', limit=fetchlim)
             npost = 0
+            domain_count = dict()
+            tdomain, tlinks, bdomain, tlinks = False,False,False,False
             for post in matched_posts:
                 postid = post.id
                 title = post.title
+                title, tdomain, tlinks = self.clean_links(title)
                 body = post.selftext
                 body = body.replace("\n"," <newline> ").replace(";", ":")
-                body, doms, full_links = self.clean_links(body)
+                body, bdomain, blinks = self.clean_links(body)
                 author = post.author
                 nsfw = post.over_18
                 ncomm = post.num_comments
                 uprat = post.upvote_ratio
                 date = datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S')
                 link = post.permalink
-                self.posts.loc[len(self.posts)] = [postid, title, body, author, nsfw, ncomm, uprat, date, link, subreddit]
+                contained_links = tlinks + blinks
                 npost += 1
-            self.description.loc[len(self.description)] = [subreddit, npost, 0]
+                domain_counts = self.dict_add(domain_count,tdomain,bdomain)
+                self.posts.loc[len(self.posts)] = [postid, title, body, author, nsfw, ncomm, uprat, date, link, subreddit, contained_links]
+            
+            self.description.loc[len(self.description)] = [subreddit, npost, 0, domain_count]
         return self.posts
 
+    def dict_add(self,dc,dict1,dict2):
+        dc=dc
+        for i,c in dict1.items():
+            if i in dc.keys():
+                dc[i] = dc[i]+c
+            else:
+                dc[i] = c
+        for i,c in dict2.items():
+            if i in dc.keys():
+                dc[i] = dc[i]+c
+            else:
+                dc[i] = c
+        return dc
+
+    def rm_dups(self):
+        """Removes duplicate posts"""
+        return None
+
     def clean_links(self, text):
+        """Removes links from text, returns clean text and list of removed links"""
         ntext = text
         # url regex
-        linkRegex = r"([http|ftp|https]+:\/\/[\w_-]+(?:(?:\.(?P<dom>[\w_-]+)\.[\w_-]+)+)[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])"
+        linkRegex = r"([http:\/\/|ftp:\/\/|https:\/\/]*[www\.]*(?P<dom>[\w_-]+)\.[\w_-]{2,3}[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])"
         # collect all links
         links = re.findall(linkRegex, ntext)
         # replace all reddit style markup links with only text
-        ntext = re.sub(r"(\[(?P<txt>[\s]*)\]\([\s]*\))", "\g<txt>", ntext)
+        ntext = re.sub(r"(\[(?P<txt>[\s]*[\S]*)\]\([\s]*[\S]*\))", "\g<txt>", ntext)
         # replace all remaining links with main part of address
         ntext = re.sub(linkRegex, "LinkTO\g<dom>", ntext)
 
@@ -111,57 +140,44 @@ class RScraper(Reddit):
 
     def gen_corpus(self, sub = False, stringlist = False):
         corpus = ""
-        links = list()
-        doms = dict()
-        
         if stringlist == True:
             corpus = []
             if sub == False:
                 for row in self.posts.iterrows():
                     txt = ""
                     txt += row[1]['title'] + " "
-                    txt += row[1]['body']
+                    txt += row[1]['body']+ " "
                     txt = txt.replace("<newline>", " ")
                     txt = (re.sub(' +', ' ',(txt.replace('\t',' ')))).strip()
-                    #txt, ls = self.clean_links(txt)
                     corpus.append(txt)
-                    if ls != []:
-                        links.append(ls)
             else:
-                for row in self.posts.loc[self.posts['sub'] == sub].iterrows():
+                for row in self.posts.loc[self.posts['subreddit'] == sub].iterrows():
                     txt = ""
-                    txt += str(row[1]['title'])
-                    txt += str(row[1]['body'])
+                    txt += str(row[1]['title'])+ " "
+                    txt += str(row[1]['body'])+ " "
                     txt = txt.replace("<newline>", " ")
                     txt = (re.sub(' +', ' ',(txt.replace('\t',' ')))).strip()
-                    txt, ds, ls = self.clean_links(txt)
                     corpus.append(txt)
-                    if ls != []:
-                        links.append(ls)
-                    for it in ds.keys():
-                        if it in doms.keys() :
-                            doms[it] = doms[it]+ds[it]
-                        else:
-                            doms[it] = ds[it]
         else:
             if sub == False:
                 for row in self.posts.iterrows():
-                    corpus += row[1]['title']
-                    corpus += row[1]['body']
+                    corpus += row[1]['title']+ " "
+                    corpus += row[1]['body']+ " "
             else:
-                for row in self.posts.loc[self.posts['sub'] == sub].iterrows():
-                    corpus += str(row[1]['title'])
-                    corpus += str(row[1]['body'])
+                for row in self.posts.loc[self.posts['subreddit'] == sub].iterrows():
+                    corpus += str(row[1]['title'])+ " "
+                    corpus += str(row[1]['body'])+ " "
             corpus = corpus.replace("<newline>", " ")
             corpus = (re.sub(' +', ' ',(corpus.replace('\t',' ')))).strip()
-            # corpus, links = self.clean_links(corpus)
-        return corpus, doms, links
+        return corpus
 
     def nlp_prep(self, txt):
-        # remove stopwords, new lines and other 
-        tokens = word_tokenize(txt)
-        stop_words = open('smart.txt','r').read().replace("\n", " ").split()
-        clean_tokens = [word for word in tokens if not word in stop_words]
+        # remove stopwords, new lines and punctuation
+        stop_words = open('smart.txt','r').read().replace("\n", " ")
+        punct  = r"[.?!,;:\-\\\(\)_']"
+        text = re.sub(punct," ", txt)
+        tokens = word_tokenize(text)
+        clean_tokens = [word for word in tokens if not word in stop_words.split()]
         # lemmatize
         lemmat = WordNetLemmatizer()
         clean_tokens = [lemmat.lemmatize(w) for w in clean_tokens]
@@ -173,34 +189,40 @@ class RScraper(Reddit):
         ntext = (" ").join(clean_tokens)
         return ntext
 
-    def freqNgrams(self, sub, lim = 3):
+    def freqNgrams(self, sub, lim = 7):
         freq_ngrams = []
         c_vec = CountVectorizer(ngram_range=(3,5))
-        corpus, doms, links = self.gen_corpus(sub=sub, stringlist=True)
+        corpus = self.gen_corpus(sub=sub, stringlist=True)
         corpus = [self.nlp_prep(c) for c in corpus]
-        ngrams = c_vec.fit_transform(corpus)
-        vocab = c_vec.vocabulary_
-        count_values = ngrams.toarray().sum(axis=0)
-        for ng_count, ng_text in sorted([(count_values[i],k) for k,i in vocab.items()], reverse=True):
-            if ng_count >= lim:
-                freq_ngrams.append((ng_text, ng_count))
-        return freq_ngrams, doms, links
+        if len(corpus) > 0:
+            ngrams = c_vec.fit_transform(corpus)
+            vocab = c_vec.vocabulary_
+            count_values = ngrams.toarray().sum(axis=0)
+            for ng_count, ng_text in sorted([(count_values[i],k) for k,i in vocab.items()], reverse=True):
+                if ng_count >= lim:
+                    freq_ngrams.append((ng_text, ng_count))
+        return dict(freq_ngrams)
 
-    def sub_mentions(self):
-        mentioned_subs = pd.DataFrame(columns=["sub", "mentions"])
-        subs = list(self.description['sub'])
-        mentions = dict()
-        
-        for s in subs:
-            text, d, links = self.gen_corpus(s)
-            ment_list = [t for t in s.split() if t.startswith('r/')]
+    def sub_mentions(self, sub='all'):
+        mentioned_subs = False
+        if sub == 'all':
+            mentioned_subs = pd.DataFrame(columns=["sub", "mentions"])
+            subs = list(self.description['subreddit'])
+            for s in subs:
+                text = self.gen_corpus(s)
+                ment_list = [t for t in s.split() if t.startswith('r/')]
+                ments = Counter(ment_list)
+                mentioned_subs.loc[len(mentioned_subs)] = [s, dict(ments)]
+        else:
+            text = self.nlp_prep(self.gen_corpus(sub)).split()
+            ment_list = [t for t in text if t.startswith('r/')]
             ments = Counter(ment_list)
-            mentioned_subs.loc[len(mentioned_subs)] = [s, dict(ments)]
+            mentioned_subs = dict(ments)
 
         return mentioned_subs
 
     def summary(self):
-        if not 'all' in list(self.description['sub']):
+        if not 'all' in list(self.description['subreddit']):
             total = self.description['n_posts'].sum()
             self.description.loc[len(self.description)] = ['all', total, 0]
         return self.description
@@ -209,18 +231,71 @@ class RScraper(Reddit):
         # Save pandas dataframes to file
         now = datetime.now()
         now = str(now)[:-7].replace(" ", "_").replace(":", "_")
-        with open(f"posts_{now}.csv", "w", encoding="utf-8") as f:
-            self.posts.to_csv(f, sep=";")
+        fname = f"posts_{now}.csv"
+        dname = f"subreddits_{now}.csv"
+        self.posts.to_csv(fname, sep=";",encoding='utf-8-sig')
+        self.description.to_csv(dname, sep=";",encoding='utf-8-sig')
         return None
 
-    def load(self, file):
-        self.posts = pd.read_csv(file, sep=';', index_col='Unnamed: 0')
-        counts = self.posts.groupby('sub')['sub'].count()
-        nms = list(counts.index)
-        n = list(counts)
-        self.description['sub'] = nms
-        self.description['n_posts'] = n
-        self.description['n_comments'] = 0
+    def load(self, subs, posts):
+        # Load previously collected posts
+        self.description = pd.read_csv(subs, delimiter=';', encoding='utf-8-sig', index_col='Unnamed: 0')
+        self.posts = pd.read_csv(posts, sep=';', index_col='Unnamed: 0')
+
+        if not 'n_posts' in list(self.description.columns):
+            counted = self.posts.groupby('subreddit')['subreddit'].count()
+            subredds = list(counted.index)
+            counts = list(counted)
+            counted = pd.DataFrame({'subreddit':subredds, 'n_posts':counts}, index=None)
+            self.description = pd.merge(self.description,counted,how='left',on='subreddit').fillna(0)
+            self.description['n_comments'] = 0
+            self.description.to_csv(subs, sep=';', encoding='utf-8-sig')
+
+        if not 'ngrams' in list(self.description.columns) and not 'sub_mention' in list(self.description.columns):
+            mentions = []
+            ngrams = []
+            for sub in list(self.description['subreddit']):
+                mentions.append(self.sub_mentions(sub))
+                ngrams.append(self.freqNgrams(sub))
+                print(f"{sub}.. done computing mentions and ngrams")
+            self.description['sub_mentions'] = mentions
+            self.description['ngrams'] = ngrams
+            self.description.to_csv(subs, sep=';', encoding='utf-8-sig')
+
+        return self.posts, self.description
+
+    def get_window(self,text, platformlist):
+        windows = []
+        for p in platformlist:
+            splittext = text.split(p)
+            if len(splittext) > 1:
+                w1,w2="",""
+                for i in range(len(splittext)):
+                    if i == 0:
+                        w1 = " ".join(splittext[i].split()[-6:])
+                    elif i == len(splittext)-1:
+                        w2 = " ".join(splittext[i].split()[:6])
+                        window = f"{w1} {p} {w2}"
+                        windows.append(window)
+                    else:
+                        w2 = " ".join(splittext[i].split()[:6])
+                        window = f"{w1} {p} {w2}"
+                        windows.append(window)
+                        w1 = " ".join(splittext[i].split()[-6:])
+        return windows
+
+    def add_windows(self, platformlist):
+        platforms = []
+        self.posts['window'] = pd.NA
+        with open(platformlist, 'r') as p:
+            platforms = p.read().replace("\n", " ").split()
+        for i in range(len(self.posts)):
+            text = str(self.posts.iloc[i]['body']).lower() 
+            if len(text) > 0:
+                w_text = self.get_window(text, platforms)
+                self.posts.iat[i,11] = w_text
+                
+        return None
 
     def fetch_comments_of_post(self, posts):
         """post.comments.replace_more(limit=None)
@@ -229,6 +304,7 @@ class RScraper(Reddit):
                 print(f"Comment: {text}")
             print()"""
         return None
+
 
 
 def main():
@@ -256,34 +332,35 @@ def main():
 
 
 if __name__ == "__main__":
-    # Input files
-    subs = "subreddits.txt"
-    plats = "platforms.txt"
+    # Pandas print setting
     pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.max_rows', None)
 
+    # Input files
+    subs = "subredditsV1.txt"
+    plats = "platformsV1.txt"
+
+    # Scraper init
     scraper = RScraper("reddit-credentials.txt")
-    data = scraper.load("posts_2023-05-01_15_28_31.csv")
+
+    #Load previous collection
+    #posts, description = scraper.load("subredditsV1_2.csv", "postsV1_2.csv")
+    posts, description = scraper.load("subreddits_vu.csv", "posts_vu.csv")
+
+    # Add windows
+    scraper.add_windows(plats)
+    scraper.posts.to_csv("postsV1_3.csv", sep=";",encoding='utf-8-sig')
+
+
+
+    # Collect new data
     #data = scraper.fetch_posts(subs,plats, 1000)
-    #data = scraper.fetch_posts(subs,"", 1000)
+    #print(data.head())
+    #print(scraper.summary())
+    #input("Save this collection? Else, abort.")
     #scraper.safe_all_to_csvs()
 
-    # INSTEAD OF PRINTING OUT, NOW THE PROGRAM SHOULD JUST BUILD THE APPROPRIATE DATAFRAME !!!
-    # THE LINKS SHOULD BE CLEANED FROM TEXT BEFORE IT IS STORED IN DF
-    # THUS THE GEN_CORPUS FUNC NEEDS TO BE CHANGED
-    
-
-    
-    mentions = scraper.sub_mentions()
-    for row in mentions.iterrows():
-        sub = row[1]['sub']
-        if sub != 'all':
-            print(sub)
-            print(row[1]['mentions'])
-            ngs, doms, links = scraper.freqNgrams(sub)
-            print(ngs)
-            print(links)
-            print(doms)
-            print()
+    #input()
 
 
 
